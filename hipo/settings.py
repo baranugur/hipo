f8090aea36e5ad00cecac2a09f3bfe5577c2685d
baranugur <baranugur7@gmail.com>
2018-12-24 16:30:10 +0300

    Switch to PostgreSQL

--- hipo/settings.py
+++ hipo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hipo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photo',
+        'USER': 'postgres',
+        'PASSWORD': '1234567890',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
